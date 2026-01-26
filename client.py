68801893725ebd4c13e9e169e5f63ed6f7b5a00e
Shahu Patil <135165337+ShahuPatil07@users.noreply.github.com>
2026-01-26 06:22:36 +0530

    Update and rename config.py to client.py

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket
+import numpy as np
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # default to localhost for local testing
+        self.host = "10.7.243.89"
+        self.port = 5555
+        self.addr = (self.host, self.port)
+
+    def connect(self, name):
+        self.client.connect(self.addr)
+
+        # inform server of username
+        self.client.send(str.encode(name))
+
+        # receive user ID from server
+        val = self.client.recv(8)
+        player_id = int(val.decode())
+        
+        # receive collision map info
+        map_info_bytes = self.client.recv(12)  # 3 int32s
+        map_info = np.frombuffer(map_info_bytes, dtype=np.int32)
+        self.grid_w, self.grid_h, self.grid_size = map_info
+        
+        # receive collision map data
+        map_size = self.grid_w * self.grid_h * 4  # int32 = 4 bytes
+        map_bytes = bytes()
+        while len(map_bytes) < map_size:
+            map_bytes += self.client.recv(4096)
+        self.collision_map = np.frombuffer(map_bytes[:map_size], dtype=np.int32).reshape((self.grid_h, self.grid_w))
+        
+        return player_id 
+
+    def disconnect(self):
+        self.client.close()
+    
+    def get_collision_map(self):
+        """Return the collision map and grid info"""
+        return self.collision_map, self.grid_w, self.grid_h, self.grid_size
+
+    def send(self, keyboard_input):
+        try:
+            client_msg = keyboard_input.tobytes()
+            self.client.send(client_msg)
+            reply = bytes()
+            while len(reply) < 2688:
+                reply += self.client.recv(1024)
+            game_world = np.frombuffer(reply, dtype=np.float64).reshape((48, 7))
+            return game_world
+        except socket.error as e:
+            print(e)
+
